d512f33f53fe9d1870d04ede72e815d61c9143fd
TheInvadr <rahulnadipalli08@gmail.com>
2026-02-23 17:55:39 +0800

    Remove stuff

--- docs/script.js
+++ docs/script.js
@@ -260,7 +260,7 @@ class LazyImageLoader {
 // Markdown content loader
 class MarkdownLoader {
     constructor() {
-        this.sections = ['about','resources'];
+        this.sections = ['about'];
         this.init();
     }
 
